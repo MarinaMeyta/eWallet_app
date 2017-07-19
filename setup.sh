9ab0e95e2b0ab86313b5ad69659130a2854c5c36
MarinaMeyta <somekindofcookiemonster@gmail.com>
2017-07-19 22:54:26 +0700

    setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install PyQt5, Qt designer for Python3:
+sudo apt-get install python3-pyqt5
+sudo apt-get install qttools5-dev-tools
+sudo apt install pyqt5-dev-tools
+
+# Qt designer binary can be found in:
+# /usr/lib/x86_64-linux-gnu/qt5/bin/designer
+
+# To convert QtDesigner ui into python code use pyuic5:
+# pyuic5 -x pyqtdesigner_test.ui -o test.py
+
+# Use pyqtdeploy for your Qt app deployment
+# pip3 install pyqtdeploy
+
+# Install QtSql dependency
+sudo apt-get install python3-pyqt5.qtsql
