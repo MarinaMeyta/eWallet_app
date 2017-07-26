10d28401ef1d27eeb1dc70d3061d42391d837419
MarinaMeyta <somekindofcookiemonster@gmail.com>
2017-07-27 00:22:20 +0700

    experimenting with encryption

--- rsa_test.py
+++ rsa_test.py
@@ -3,7 +3,7 @@ from Crypto import Random
 
 random_generator = Random.new().read
 PIN = "456431"
-private_key = RSA.generate(2048, random_generator) 
+private_key = RSA.generate(256, random_generator) 
 exported_key = private_key.exportKey('PEM', PIN, pkcs=1)
 f = open('privatekey.pem','wb')
 f.write(bytes(exported_key)) 
@@ -13,7 +13,14 @@ f.close()
 f = open('privatekey.pem','r')
 key = RSA.importKey(f.read(), PIN)
 pub_key = key.publickey()
-print(pub_key)
+exported_key = pub_key.exportKey('PEM')
+f = open('pubkey.pem','wb')
+f.write(bytes(exported_key)) 
+f.close()
+
+
+
 
 if private_key == key:
     print("True") 
+
