78d1405add95b9e05043face9dcb1bc199a29768
MarinaMeyta <somekindofcookiemonster@gmail.com>
2017-08-20 00:31:40 +0700

    token object for PIN check+ID

--- src/token.py
+++ src/token.py
@@ -1,29 +1,43 @@
 import uuid
 import crypt
 import json
+import settings
+import os
 from hmac import compare_digest as compare_hash
 
 
 class Token():
     def __init__(self, PIN):
-        self.id = self.generate_id()
-        self.salt = self.generate_salt(PIN)
-        self.hash = self.generate_hash(PIN, self.salt)
-        self.save()
+        try:
+            with open(os.path.join(settings.PATH_TO_TOKEN, 'TOKEN')) as token_file:
+                data = json.load(token_file)
+                self.id = data["id"]
+                self.salt = data['salt']
+                self.hash = data['hash']
+        except IOError:
+            self.id = self.generate_id()
+            self.salt = self.generate_salt(PIN)
+            self.hash = self.generate_hash(PIN, self.salt)
+            self.save()
+
 
     def generate_id(self):
         return uuid.uuid4().hex
 
+
     def generate_salt(self, PIN):
         return crypt.mksalt(crypt.METHOD_SHA512)
+
 
     def generate_hash(self, PIN, salt):
         return crypt.crypt(PIN, salt)
 
+
     def save(self):
-        with open('TOKEN', 'w') as hash_file:
+        with open(os.path.join(settings.PATH_TO_TOKEN, 'TOKEN'), 'w') as hash_file:
             data = {'id': self.id, 'salt': self.salt, 'hash': self.hash}
             json.dump(data, hash_file, sort_keys=True, indent=4)
+
 
     def check_PIN(self, PIN):
         return compare_hash(crypt.crypt(PIN, self.hash), self.hash)
