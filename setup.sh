5b13550c334512579be3409cdfb6f863a886fca5
MarinaMeyta <somekindofcookiemonster@gmail.com>
2017-08-21 20:44:00 +0700

    requirements for virtualenv

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Install PyQt5, Qt designer for Python3:
-sudo apt-get install python3-pyqt5
-sudo apt-get install qttools5-dev-tools
-sudo apt install pyqt5-dev-tools
-
 # Qt designer binary can be found in:
 # /usr/lib/x86_64-linux-gnu/qt5/bin/designer
 
@@ -15,9 +10,9 @@ sudo apt install pyqt5-dev-tools
 # pip3 install pyqtdeploy
 
 # Install QtSql dependency
-sudo apt-get install python3-pyqt5.qtsql
+#sudo apt-get install python3-pyqt5.qtsql
 
 # PyCrypto library for AES-256 usage
-pip3 install pycrypto
+#pip3 install pycrypto
 
 # install pytest
