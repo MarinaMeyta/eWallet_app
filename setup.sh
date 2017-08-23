a81a31c1a9875b1005e8c46b429ad7d7e360ee36
MarinaMeyta <somekindofcookiemonster@gmail.com>
2017-08-23 23:28:56 +0700

    setup.sh updated

--- setup.sh
+++ setup.sh
@@ -6,13 +6,5 @@
 # To convert QtDesigner ui into python code use pyuic5:
 # pyuic5 -x pyqtdesigner_test.ui -o test.py
 
-# Use pyqtdeploy for your Qt app deployment
-# pip3 install pyqtdeploy
-
-# Install QtSql dependency
-#sudo apt-get install python3-pyqt5.qtsql
-
-# PyCrypto library for AES-256 usage
-#pip3 install pycrypto
-
-# install pytest
+# run pytest without import errors
+python -m pytest -v tests/
